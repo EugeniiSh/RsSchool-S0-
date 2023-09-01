bdbae3f716f35ac22f1173a98e0a7476721ce211
EugeniiSh <evgenii.shmerko@mail.ru>
2023-09-01 14:59:52 +0300

    feat: add new functional, from profile also

--- library/js/indexLocal.js
+++ library/js/indexLocal.js
@@ -1,25 +1,36 @@
+// --- Окно логина ---
 const loginNameInput = document.querySelector('.login-form__input-email');
 const loginPasswordInput = document.querySelector('.login-form__input-password');
-
+// --- Окно регистрации ---
 const firstNameRegInput = document.querySelector('.register-form__input-first-name');
 const lastNameRegInput = document.querySelector('.register-form__input-last-name');
 const eMailRegInput = document.querySelector('.register-form__input-email');
 const passwordRegInput = document.querySelector('.register-form__input-password');
-
+// --- Кнопки submit ---
 const loginFormBtn = document.querySelector('.login-form__btn');
 const registerFormBtn = document.querySelector('.register-form__btn');
+// --- Иконка копирования card number ---
+const copyCardNumber = document.querySelector('.profile-card-number__copide');
 
 console.log(JSON.parse(localStorage.getItem('userInfo')));
 
 function loginUser ()
 {
-    const loginUserInfo = JSON.parse(localStorage.getItem('userInfo'));
+    let loginUserInfo = JSON.parse(localStorage.getItem('userInfo'));
 
     if (((loginNameInput.value === loginUserInfo.eMail)
      || (loginNameInput.value === loginUserInfo.cardNumber))
      && (loginPasswordInput.value === loginUserInfo.password))
     {
         localStorage.setItem('loginStatus', 'true');
+        loginUserInfo.visits = +loginUserInfo.visits + 1;
+        localStorage.setItem('userInfo', JSON.stringify(loginUserInfo));
+
+        modalWindow.classList.remove('active-modal');
+        modalLogin.classList.remove('active-modal');
+
+        getIconProfile ()
+        location.reload()
     } 
     else
     {
@@ -31,15 +42,21 @@ function registerNewUser ()
 {
     if((firstNameRegInput.value != '') && 
     (lastNameRegInput.value != '') &&
+    (eMailRegInput.validity.valid) &&
     (passwordRegInput.value.length > 7))
     {
         const newUser = {};
 
         newUser.firsName = firstNameRegInput.value;
         newUser.lastName = lastNameRegInput.value;
+        newUser.initials = [firstNameRegInput.value[0].toUpperCase(), lastNameRegInput.value[0].toUpperCase()]
         newUser.eMail = eMailRegInput.value;
         newUser.password = passwordRegInput.value;
         newUser.cardNumber = Math.random().toString(16).slice(2,11).toUpperCase();
+        newUser.visits = 1;
+        newUser.books = {};
+        newUser.books.rentBooks = [];
+        newUser.books.countBooks = newUser.books.rentBooks.length;
 
         localStorage.setItem('loginStatus', 'true');
         localStorage.setItem('userInfo', JSON.stringify(newUser));
@@ -47,10 +64,78 @@ function registerNewUser ()
         modalWindow.classList.remove('active-modal');
         modalLogin.classList.remove('active-modal');
 
-        console.log(newUser);
+        getIconProfile ();
+        location.reload()
     }
     
 };
 
+function getIconProfile ()
+{
+    if(localStorage.getItem('loginStatus') === 'true')
+    {
+        const loginUserInfo = JSON.parse(localStorage.getItem('userInfo'));
+
+        // -- Замена иконки профиля -- 
+        document.querySelector('.heading__login-profile').classList.add('active__heading-login-profile');
+        document.querySelector('.heading__login-profile').textContent = loginUserInfo.initials[0] + loginUserInfo.initials[1];
+        loginImg.setAttribute('title', `${loginUserInfo.firsName} ${loginUserInfo.lastName}`);
+        // -- Замена информации в профиле --
+        modalProfile.querySelector('.block-avatar').textContent = loginUserInfo.initials[0] + loginUserInfo.initials[1];
+        modalProfile.querySelector('.block-name').textContent = loginUserInfo.firsName + ' ' + loginUserInfo.lastName;
+        modalProfile.querySelector('.achievement-value-visits').textContent = loginUserInfo.visits;
+        modalProfile.querySelector('.profile-card-number__value').textContent = loginUserInfo.cardNumber;
+    }
+    else
+    {
+        // -- Возврат стандартной иконки профиля --
+        document.querySelector('.heading__login-profile').classList.remove('active__heading-login-profile'); 
+        loginImg.removeAttribute('title');
+
+    }
+};
+
+// --- Копирование card number в профиле ---
+copyCardNumber.addEventListener('click',
+    function ()
+    {
+        if (navigator.clipboard) 
+        {
+            const loginUserInfo = JSON.parse(localStorage.getItem('userInfo'));
+            navigator.clipboard.writeText(loginUserInfo.cardNumber).then(function() 
+            {
+                console.log('Текст успешно скопирован в буфер обмена');
+                copyCardNumber.classList.add('copide-active');
+                setTimeout(() => copyCardNumber.classList.remove('copide-active'), 250);
+            }, 
+            function(err) 
+            {
+                console.error('Произошла ошибка при копировании текста: ', err);
+            });
+        } 
+        else 
+        {
+            alert('Копирование не доступно!');
+        }
+    }
+);
+
+
 loginFormBtn.onclick = loginUser;
 registerFormBtn.onclick = registerNewUser;
+getIconProfile ();
+
+// window.addEventListener('storage', event => 
+// {
+//     console.log(event);
+// });
+
+// window.addEventListener('click', (event) => 
+// {
+//     console.log(event.target);
+// });
+
+// modalProfile
+
+
+
--- library/js/indexModal.js
+++ library/js/indexModal.js
@@ -1,3 +1,4 @@
+// --- Дроп меню профиля в хедере ---
 const loginImg = document.querySelector('.login-img');
 const dropMenuProfile = document.querySelector('.modal-menu__profile-no-auth');
 const dropMenuProfileHeader = document.querySelector('.profile-no-auth__header');
@@ -5,14 +6,16 @@ const dropMenuProfileLogin = document.querySelector('.profile-no-auth__log-in');
 const dropMenuProfileRegister = document.querySelector('.profile-no-auth__register');
 const dropMenuProfileMyProfile = document.querySelector('.profile-no-auth__my-profile');
 const dropMenuProfileLogOut = document.querySelector('.profile-no-auth__log-out');
-
+// --- Все модальные окна ---
 const modalWindow = document.querySelector('.modal-window');
 const modalLogin = document.querySelector('.modal__log-in');
 const modalRegister = document.querySelector('.modal__register');
 const modalProfile = document.querySelector('.modal-profile');
 const modalBuyCard = document.querySelector('.modal-buy-card');
-// const modalBtn = document.querySelectorAll('.modal-btn');
-// const modalClose = document.querySelectorAll('.modal-close');
+// --- Кнопки из Digital Library Cards ---
+const digitalLogin = document.querySelector('.card-button__log-in');
+const digitaSignUp = document.querySelector('.card-button__sign-up');
+const digitaProfile = document.querySelector('.card-button__profile');
 
 // localStorage.setItem('loginStatus', 'false');
 
@@ -22,6 +25,7 @@ loginImg.addEventListener('click',
     function ()
     {
         dropMenuProfile.classList.toggle('profile-menu__active');
+        const loginUserInfo = JSON.parse(localStorage.getItem('userInfo'));
 
         if (localStorage.getItem('loginStatus') === 'true')
         {
@@ -29,6 +33,9 @@ loginImg.addEventListener('click',
             dropMenuProfileRegister.style = 'display: none';
             dropMenuProfileMyProfile.style = 'display: ""';
             dropMenuProfileLogOut.style = 'display: ""';
+
+            dropMenuProfileHeader.textContent = loginUserInfo.cardNumber;
+            dropMenuProfileHeader.style = 'font-size: 13px';
         }
         else
         {
@@ -36,6 +43,9 @@ loginImg.addEventListener('click',
             dropMenuProfileRegister.style = 'display: ""';
             dropMenuProfileMyProfile.style = 'display: none';
             dropMenuProfileLogOut.style = 'display: none';
+
+            dropMenuProfileHeader.textContent = 'Profile';
+            dropMenuProfileHeader.style = 'font-size: 15px';
         }
     }
 );
@@ -50,7 +60,7 @@ window.addEventListener('click', (event) =>
         dropMenuProfile.classList.remove('profile-menu__active')
     }    
 });
-
+// --- ----- ---
 
 // +++ Log In +++
 // --- Вызов ---
@@ -59,6 +69,12 @@ dropMenuProfileLogin.addEventListener('click',
     {
         modalWindow.classList.add('active-modal');
         modalLogin.classList.add('active-modal');
+        // --очистка строк ввода--
+        const LoginInputs = modalLogin.querySelectorAll('.modal-input');
+        for(let elem of LoginInputs)
+        {
+            elem.value = '';
+        }
     }
 );
 
@@ -78,7 +94,7 @@ window.addEventListener('click', (event) =>
         modalRegister.classList.add('active-modal');
     }
 });
-
+// --- ----- ---
 
 // +++ Register +++
 // --- Вызов ---
@@ -112,6 +128,74 @@ window.addEventListener('click', (event) =>
         modalRegister.classList.remove('active-modal');
     }
 });
+// --- ----- ---
+
+// +++ Profile +++
+// --- Вызов ---
+dropMenuProfileMyProfile.addEventListener('click', 
+    function()
+    {
+        modalWindow.classList.add('active-modal');
+        modalProfile.classList.add('active-modal');
+    }
+);
+
+// --- Закрытие ---
+window.addEventListener('click', (event) => 
+{
+    if((event.target == document.querySelector('.profile-header__close')) ||
+    (event.target == modalWindow))
+    {
+        modalWindow.classList.remove('active-modal');
+        modalProfile.classList.remove('active-modal')
+    } 
+    
+    if (event.target == document.querySelector('.register-footer__link'))
+    {
+        modalLogin.classList.add('active-modal');
+        modalProfile.classList.remove('active-modal');
+    }
+});
+// --- ----- ---
+
+// +++ Вызовы из Digital Library Cards +++
+// --- Log In ---
+digitalLogin.addEventListener('click',
+    function ()
+    {
+        modalWindow.classList.add('active-modal');
+        modalLogin.classList.add('active-modal');
+        // --очистка строк ввода--
+        const LoginInputs = modalLogin.querySelectorAll('.modal-input');
+        for(let elem of LoginInputs)
+        {
+            elem.value = '';
+        }
+    }
+);
+// --- Register ---
+digitaSignUp.addEventListener('click',
+    function ()
+    {
+        modalWindow.classList.add('active-modal');
+        modalRegister.classList.add('active-modal');
+        // --очистка строк ввода--
+        const registerInputs = modalRegister.querySelectorAll('.modal-input');
+        for(let elem of registerInputs)
+        {
+            elem.value = '';
+        }
+    }
+);
+// --- Profile ---
+digitaProfile.addEventListener('click',
+    function ()
+    {
+        modalWindow.classList.add('active-modal');
+        modalProfile.classList.add('active-modal');
+    }
+);
+// --- ----- ---
 
 // +++ Log Out +++
 // --- Вызов ---
@@ -119,5 +203,9 @@ dropMenuProfileLogOut.addEventListener('click',
     function ()
     {
         localStorage.setItem('loginStatus', 'false');
+
+        getIconProfile ()
+        location.reload();
     }
 );
+// --- ----- ---
