8e54a1d06a42276cceba0ee5fdd3aac1de811d2d
EugeniiSh <evgenii.shmerko@mail.ru>
2023-09-20 16:36:42 +0300

    feat: add logic for play/pause/next/volume

--- js301.2-audio-player/main.js
+++ js301.2-audio-player/main.js
@@ -1,14 +1,20 @@
 const player = document.querySelector('.player'),
       prevBtn = document.querySelector('.prev'),
       playBtn = document.querySelector('.play'),
-      nextBtn = document.querySelector('.prev'),
+      nextBtn = document.querySelector('.next'),
       audio = document.querySelector('.audio'),
       progressContainer = document.querySelector('.progress-container'),
       progress = document.querySelector('.progress'),
       tittleContent = document.querySelector('.tittle-content'),
       sliderImg = document.querySelector('.slider-img'),
       imgSrc = document.querySelector('.img__src'),
-      mainPage = document.querySelector('.main-page')
+      mainPage = document.querySelector('.main-page'),
+      totalTime = document.querySelector('.total-time'),
+      curentTime = document.querySelector('.curent-time'),
+      volumeImg = document.querySelector('.volume-img'),
+      volumeSlider = document.querySelector('.volume-slider'),
+      volumePercentage = document.querySelector('.volume-percentage')
+
 
 // Названия песен
 const songs = 
@@ -27,7 +33,19 @@ function loadSong (song)
     audio.src = `assets/audio/${song}.mp3`;
     sliderImg.src = `assets/img/cover${songIndex + 1}.jpg`;
     mainPage.style.backgroundImage = `url(assets/img/bgi${songIndex + 1}.jpg)`;
+    
 }
+
+audio.addEventListener("loadeddata", () => 
+{
+    totalTime.textContent = getTimeCodeFromNum(audio.duration);
+
+    let newVolume;
+    audio.volume = 0.02;
+    newVolume = audio.volume;
+    volumePercentage.style.height = newVolume * 100 + '%';
+},
+false);
 
 loadSong (songs[songIndex]);
 
@@ -52,10 +70,118 @@ playBtn.addEventListener('click', () =>
     const isPlaing = player.classList.contains('plays');
     if (isPlaing)
     {
-        pauseSong ()
+        pauseSong ();
     }
     else
     {
-        playSong ()
+        playSong ();
     }
 })
+
+// Next song
+function nextSong ()
+{
+    songIndex++;
+
+    if (songIndex > songs.length - 1)
+    {
+        songIndex = 0;
+    }
+
+    loadSong (songs[songIndex]);
+    playSong ();
+}
+
+nextBtn.addEventListener('click', nextSong);
+
+// Prev song
+function prevSong ()
+{
+    songIndex--;
+
+    if (songIndex < 0)
+    {
+        songIndex = songs.length - 1;
+    }
+
+    loadSong (songs[songIndex]);
+    playSong ();
+}
+
+prevBtn.addEventListener('click', prevSong);
+
+// Progress Bar & Curent time
+function getTimeCodeFromNum(num) 
+{
+    let seconds = parseInt(num);
+    let minutes = parseInt(seconds / 60);
+    seconds -= minutes * 60;
+    const hours = parseInt(minutes / 60);
+    minutes -= hours * 60;
+  
+    if (hours === 0)
+    {
+      return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
+    }  
+    
+    return `${String(hours).padStart(2, 0)}:${minutes}:${String(seconds % 60).padStart(2, 0)}`;
+}
+
+function updateProgress (e)
+{
+    const duration = e.srcElement.duration;
+    const currentTime = e.srcElement.currentTime;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+
+    curentTime.textContent = getTimeCodeFromNum(audio.currentTime);
+}
+
+audio.addEventListener('timeupdate', updateProgress);
+
+// Set Progress
+function setProgress(e)
+{
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+}
+
+progressContainer.addEventListener('click', setProgress);
+
+// Autoplay
+audio.addEventListener('ended', nextSong);
+
+// Volume change
+function changeVolume (e)
+{
+    const sliderHeight = window.getComputedStyle(volumeSlider).height;
+    const newVolume = (parseInt(sliderHeight) - e.offsetY) / parseInt(sliderHeight);
+    audio.volume = newVolume;
+    volumePercentage.style.height = newVolume * 100 + '%';
+}
+
+volumeSlider.addEventListener('click', changeVolume);
+
+// Volume mute
+function muteVolume ()
+{
+    audio.muted = !audio.muted;
+    if (audio.muted)
+    {
+        volumeImg.src = 'assets/img/volOff.svg';
+    }
+    else
+    {
+        volumeImg.src = 'assets/img/volOn.svg';
+    }
+}
+
+volumeImg.addEventListener('click', muteVolume);
+
+
+
+
+
