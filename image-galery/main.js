cd876c44a81c2af8d8b18c6813728e5d1a3af56a
EugeniiSh <evgenii.shmerko@mail.ru>
2023-09-27 14:35:47 +0300

    feat: add new functional and footer

--- image-galery/main.js
+++ image-galery/main.js
@@ -1,8 +1,23 @@
-const apiKey = 'vFRyoip0ey8MbVSV4TeDyNEhm726HdL43ekbIXIUih4';
-const urlRandom = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=4`;
-const urlSearch = `https://api.unsplash.com/search/photos?page=1&query=office&client_id=${apiKey}`;
+console.log();
+console.log('1.Вёрстка +10 (10)');
+console.log('2.При загрузке приложения на странице отображаются полученные от API изображения +10 (10)');
+console.log('3.Если в поле поиска ввести слово и отправить поисковый запрос, на странице отобразятся изображения соответствующей тематики, если такие данные предоставляет API +10 (10)');
+console.log('4.Поиск +30 (30)');
+console.log('5.Очень высокое качество оформления приложения и/или дополнительный не предусмотренный в задании функционал, улучшающий качество приложения.');
+console.log('На усмотрение проверяющего.');
 
-async function getImages()
+const apiKey = 'vFRyoip0ey8MbVSV4TeDyNEhm726HdL43ekbIXIUih4';
+const urlRandom = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=10`;
+const defaulUrlSearch = `https://api.unsplash.com/search/photos?page=1&query=programming&client_id=${apiKey}`;
+const searchUrl = `https://api.unsplash.com/search/photos?page=1&query=`;
+
+const headerForm = document.querySelector('.header-form');
+const headerSearch = document.querySelector('.header-search');
+const headerSearchCross = document.querySelector('.header-search__cross');
+
+getImages(defaulUrlSearch);
+
+async function getImages(url)
 {
     try
     {
@@ -17,10 +32,25 @@ async function getImages()
         // const data = await response.json();
         // console.log(data);
         // showImages(data);
-        const response = await fetch(urlSearch);
+        const response = await fetch(url);
         const data = await response.json();
-        console.log(data);
-        showImages(data)
+        // console.log(data);
+
+        if(data.results.length === 0)
+        {
+            const imagesEl = document.querySelector('.images');
+            imagesEl.innerHTML = ''; //Очистка предыдущего запроса.
+
+            const imageEl = document.createElement('div');
+            imageEl.classList.add('wrong');
+            imageEl.innerHTML = '<div>Something wrong. Try again</div>';
+            imagesEl.appendChild(imageEl);
+        }
+        else
+        {
+            showImages(data)
+        }
+        
     }
     catch(error)
     {
@@ -28,11 +58,12 @@ async function getImages()
     }
 }
 
-getImages();
+
 
 function showImages(data)
 {
     const imagesEl = document.querySelector('.images');
+    imagesEl.innerHTML = ''; //Очистка предыдущего запроса.
 
     data.results.forEach((content) => 
     {
@@ -48,13 +79,29 @@ function showImages(data)
         </div>
 
         <div class="image-block__info">
-            <div class="image-autor">People</div>
-            <div class="image-name">formr</div>
-            <div class="image-likes">555</div>
+            <div class="image-autor">${content.user.name}</div>
+            <div class="image-name">${content.description || ''}</div>
+            <div class="image-likes">${content.likes}</div>
         </div>`;
         imagesEl.appendChild(imageEl);
     })
 }
+
+headerForm.addEventListener('submit', (e) =>
+{
+    e.preventDefault();
+
+    const apiSearchUrl = `${searchUrl}${headerSearch.value}&client_id=${apiKey}`;
+    if (headerSearch.value)
+    {
+        getImages(apiSearchUrl)
+    }
+});
+
+headerSearchCross.addEventListener('click', () =>
+{
+    headerSearch.value = '';
+});
 
 
 // const button = document.querySelector('.btn'),
